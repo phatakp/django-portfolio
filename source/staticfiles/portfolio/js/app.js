e746b4d22f06d9cf64ecb26d20eceeaddf07db9c
Praveen Phatak <praveenphatak@gmail.com>
2020-07-30 15:55:31 +0530

    static files

--- source/staticfiles/portfolio/js/app.js
+++ source/staticfiles/portfolio/js/app.js
@@ -1,1 +1,11 @@
-new WOW().init();
+$(document).ready(function () {
+    new WOW().init();
+    $('.form-control').focus(function () {
+        console.log($(this));
+        $inputSelected = $(this);
+        $inputSelected.attr('placeholder', '');
+        $parentGroup = $inputSelected.parent().parent();
+        $parentGroup.addClass('active');
+
+    });
+});
